80febb978c557231799df256c928c0701a40277c
Lingfey <bokanchalangman@gmail.com>
2022-09-29 04:21:30 +0800

    Factorial
    Recursion

---
+++ Homework6_3_Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Homework6_3_Factorial
+{
+    internal class Program
+    {
+        static int Factorial(int number)
+        {
+            if (number == 1)
+                return 1;
+            return number * Factorial(number - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Factorial of {a} is {Factorial(a)}");
+            Console.ReadKey();
+        }
+    }
+}
